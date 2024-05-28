b64b5b704e6587a36a083f114d3386973ac3874f
Maria Clara <clarinhacarniellicoelho@gmail.com>
2024-05-28 16:25:04 -0300

    aula dia 28/05

--- app/src/main/java/com/senai/lugaluga/view/CadastroUsuarioActivity.java
+++ app/src/main/java/com/senai/lugaluga/view/CadastroUsuarioActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.senai.lugaluga.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
     private Spinner spinnerUf;
+
+    private TextInputLayout inputCpf;
+
+    private TextInputLayout inputCep;
+
+    private TextInputLayout inputData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,169 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setTitle("Cadastro");
         setSupportActionBar(myToolbar);
+
+        inputCpf = findViewById(R.id.input_cpf);
+
+        inputCep = findViewById(R.id.input_cep);
+
+        inputData = findViewById(R.id.input_data);
+
+        inputData.getEditText().addTextChangedListener(new TextWatcher() {
+
+            private static final String maskDATA = "##/##/####";
+
+            boolean isUpdating;
+
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*","");
+                String mask = maskDATA;
+
+                String mascara = "";
+                if (isUpdating){
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+                    }
+
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                inputData.getEditText().setText(mascara);
+                inputData.getEditText().setSelection(mascara.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputCep.getEditText().addTextChangedListener(new TextWatcher() {
+
+            private static final String maskCEP = "#####-###";
+
+            boolean isUpdating;
+
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*","");
+                String mask = maskCEP;
+
+                String mascara = "";
+                if (isUpdating){
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+                    }
+
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                inputCep.getEditText().setText(mascara);
+                inputCep.getEditText().setSelection(mascara.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+
+        inputCpf.getEditText().addTextChangedListener(new TextWatcher() {
+
+            private static final String maskCPF = "###.###.###-##";
+
+            boolean isUpdating;
+
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*","");
+                String mask = maskCPF;
+
+                String mascara = "";
+                if (isUpdating){
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+                    }
+
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                inputCpf.getEditText().setText(mascara);
+                inputCpf.getEditText().setSelection(mascara.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         spinnerUf = (Spinner) findViewById(R.id.spinner_uf);
 
